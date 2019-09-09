9c6abc6693fe1586f4884ff7870b9132a832b1b1
Daniel Tymecki <daniel.tymecki@gmail.com>
2019-09-09 17:50:41 -0400

    Black Box retirement testing done

--- Retirement.py
+++ Retirement.py
@@ -16,3 +16,20 @@ def Retirement(age,salary,saved,goal):
         raise TypeError('Argument saved percentage must be passed as an float of type ##%')
     if not isinstance(goal,int):
         raise TypeError('Argument goal must be passed as an integer')
+
+    # Next we test if the data makes sense given the domain
+    # Because the earliest legel age to work is 14, anything below that is invalid input
+    # Death is assumed at age 100 so anything above that is invalid
+    if age < 14 or age >= 100:
+        raise RuntimeError('Argument age must be between [14,100)')
+    # Salary is required to be above 0 
+    if salary <= 0:
+        raise RuntimeError('Argument salary must be greater than 0')
+    # Saved percentage must be bewteen [0,100]
+    if saved < 0.0 or saved > 100.00:
+        raise RuntimeError('Argument age must be between [0,100]')
+    # Your goal must be non-negative
+    if goal < 0:
+        raise RuntimeError('Argument goal must be greater than or equal to 0')
+
+    return ''
--- Retirement_test.py
+++ Retirement_test.py
@@ -3,17 +3,29 @@ import pytest
 
 def test_Retirement_age_raises_exception_on_non_int_args():
     with pytest.raises(TypeError):
-        Retirement("age",45000,5000,50000)
-        Retirement(12.2,45000,5000,50000)
-        Retirement('5',45000,5000,50000)
-        Retirement(None,45000,5000,50000)
+        Retirement("age",45000,5.0,50000)
+        Retirement(12.2,45000,5.0,50000)
+        Retirement('5',45000,5.0,50000)
+        Retirement(None,45000,5.0,50000)
+
+def test_Retirement_age_raises_exception_on_bad_int_args():
+    with pytest.raises(RuntimeError):
+        Retirement(0,45000,5.0,50000)
+        Retirement(-45,45000,5.0,50000)
+        Retirement(13,45000,5.0,50000)
+        Retirement(100,45000,5.0,50000)
 
 def test_Retirement_salary_raises_exception_on_non_int_args():
     with pytest.raises(TypeError):
-        Retirement(32,'salary',5000,50000)
-        Retirement(32,45000.24,5000,50000)
-        Retirement(32,'4',5000,50000)
-        Retirement(32,None,5000,50000)
+        Retirement(32,'salary',5.0,50000)
+        Retirement(32,45000.24,5.0,50000)
+        Retirement(32,'4',5.0,50000)
+        Retirement(32,None,5.0,50000)
+
+def test_Retirement_salary_raises_exception_on_bad_int_args():
+    with pytest.raises(RuntimeError):
+        Retirement(32,-1000,5.0,50000)
+        Retirement(32,0,5.0,50000)
 
 def test_Retirement_saved_raises_exception_on_non_float_args():
     with pytest.raises(TypeError):
@@ -22,9 +34,21 @@ def test_Retirement_saved_raises_exception_on_non_float_args():
         Retirement(32,45000,'5',50000)
         Retirement(32,45000,None,50000)
 
+def test_Retirement_saved_raises_exception_on_bad_float_args():
+    with pytest.raises(RuntimeError):
+        Retirement(32,45000,-15.2,50000)
+        Retirement(32,45000,101.5,50000)
+
 def test_Retirement_goal_raises_exception_on_non_int_args():
     with pytest.raises(TypeError):
-        Retirement(32,45000,5000.0,'goal')
-        Retirement(32,45000,5000.0,50000.25)
-        Retirement(32,45000,5000.0,'5')
-        Retirement(32,45000,5000.0,None)
+        Retirement(32,45000,5.0,'goal')
+        Retirement(32,45000,5.0,50000.25)
+        Retirement(32,45000,5.0,'5')
+        Retirement(32,45000,5.0,None)
+
+def test_Retirement_goal_raises_exception_on_bad_int_args():
+    with pytest.raises(RuntimeError):
+        Retirement(32,45000,5.0,-5)
+
+def test_Retirement_returns_string_argument():
+    assert isinstance(Retirement(32,45000,5.0,500000),str),'Test Failed: Function must return a string'
